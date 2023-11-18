432f965f61a3b9afd68fb7265f377aa913e11947
lalithrajrayappa <149830737+lalithrajrayappa@users.noreply.github.com>
2023-11-18 22:51:11 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,31 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20, ERC20Burnable, Ownable {
-    
-    string public shop_items;
-    
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
-        shop_items="These items are available to be redeemed: 1. shoes 2. laptop 3. mobile ";
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
+    string public degen_igstore;
+    string public details;
+    constructor() ERC20("Degen", "DGN") {
+        degen_igstore="The Official Degen store has the following redeemable items: 1. Degen Lamp 2. Degen Toy 3. Degen Console ";
+        details="Welcome to the Official Degen Game. Happy Gaming!!";
     }
 
-    function mint(address to_address, uint256 amt) public onlyOwner {
-        _mint(to_address, amt);
-        
-    }
-
-
-    function redeem(uint256 product_no) public{
-        
-        if (balanceOf(msg.sender)< 2000){
-            revert("Minimum balance required to redeem is 2000");
+        function mint(address to, uint256 amount) public onlyOwner {
+            _mint(to, amount);
         }
-        assert(product_no>0 && product_no<3);
-        _burn(msg.sender, product_no*2000);
-       
-    }
+
+        function redeem(uint256 redeem_value) public {
+            if(balanceOf(msg.sender)<1000){
+                revert("Insifficient Balance");
+            }
+            assert(redeem_value>0 && redeem_value<4);
+            _burn(msg.sender, redeem_value*250);
+        }
+
+        function doublemint(uint256 amount) public {
+            require(balanceOf(msg.sender)>500 && balanceOf(msg.sender)>1000, "You can double mint only within this range");
+            _mint(msg.sender, amount*2);
+
+        }
 }
